33c0a4cbf7ed0d528619db384bcf1cee0670c2d9
victorwrage <wrage@126.com>
2020-02-27 09:49:16 +0800

    plugin xiangmu

---
+++ sample-runtime/src/main/java/com/tencent/shadow/sample/runtime/PluginDefaultProxyActivity.java
@@ -0,0 +1,15 @@
+package com.tencent.shadow.sample.runtime;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.tencent.shadow.core.runtime.container.PluginContainerActivity;
+
+public class PluginDefaultProxyActivity extends PluginContainerActivity {
+    @Override
+    public View onCreateView(String arg0, Context arg1, AttributeSet arg2) {
+        return super.onCreateView(arg0, arg1, arg2);
+    }
+}
